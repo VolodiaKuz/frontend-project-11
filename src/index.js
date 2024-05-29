dbfc4a9cc698f0dc03e82aaffdce3c316b63e224
Volodia K <wtraktorman94@mail.ru>
2024-05-29 23:56:27 +0500

    try to import yum and onchange

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 // import './styles.scss';
 // import 'bootstrap';
 
-// const test = () => console.log('it worked!');
+// import * as yup from 'yup';
+// import onChange from 'on-change';
+
+// import yup from '../node_modules/yup/index.js';
+// import { onchange } from '../node_modules/onchange/dist/index.js';
+
+// const schema = yup.object().shape({
+//   input: yup.string().trim().required(),
+// });
+
+// const validate = (fields) => {
+//   try {
+//     schema.validateSync(fields, { abortEarly: false });
+//     return {};
+//   } catch (e) {
+//     return keyBy(e.inner, 'path');
+//   }
+// };
+
+// const watchedState = onChange(state, (path, value) => {
+//   const validation = validate(state.registrationForm.data);
+
+//   if (Object.keys(validation).length === 0) console.log('Object.keys(validation).length === 0');
+
+// });
+
+
 
 const init = () => {
   const state = {
     rss: ['hey'],
     url: '',
+    form: {
+      valid: true,
+      errors: {},
+      fields: {
+        input: '',
+      },
+    }
   };
   return state;
 };
@@ -31,7 +64,7 @@ export default () => {
   elements.submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     // watchedState.registrationForm.data.password = e.target.value;
-    state.rss.push('submit')
+    state.rss.push(elements.input.value)
     console.log(state.rss);
 
     const test = document.createElement('p');
@@ -40,20 +73,29 @@ export default () => {
     // text-white
     elements.form.after(test)
     elements.form.append(test)
+
+    elements.input.value = '';
+    console.log(elements.input);
+    elements.input.focus();
+
+    // const errors = validate(state.form.fields);
+    // console.log(errors);
+    // watchedState.registrationForm.data.name = e.target.value;
+
   });
 
-  elements.submitButton.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // // console.log(elements.input.value);
-    // state.rss.push('submit')
-    // console.log(state.rss);
-    // // watchedState.registrationForm.data.password = e.target.value;
-    // console.log('button');
-    const test = document.createElement('p');
-    test.textContent = 'submit';
-    elements.form.after(test)
-    elements.form.append(test)
-  });
+  // elements.submitButton.addEventListener('submit', (e) => {
+  //   e.preventDefault();
+  //   // // console.log(elements.input.value);
+  //   // state.rss.push('submit')
+  //   // console.log(state.rss);
+  //   // // watchedState.registrationForm.data.password = e.target.value;
+  //   // console.log('button');
+  //   const test = document.createElement('p');
+  //   test.textContent = 'submit';
+  //   elements.form.after(test)
+  //   elements.form.append(test)
+  // });
 
 }
 
