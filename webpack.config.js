842ebe1a050553f78c981dd959d4c462d1da10ff
Volodia K <wtraktorman94@mail.ru>
2024-06-05 13:41:16 +0500

    change entry point in wp.config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 
 
 const config = {
-    entry: './src/index.js',
+    entry: './src/init.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         // path: path.resolve('./', 'public'),
